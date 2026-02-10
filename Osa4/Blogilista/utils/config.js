e7c5db34e8d4f42982e8807317e3b2084e700a91
NeaS <nea.sivonen8@gmail.com>
2026-02-10 11:25:11 +0200

    Refactor config.js to improve security
    Updated MongoDB URI to use a placeholder for the password.

--- Osa4/Blogilista/utils/config.js
+++ Osa4/Blogilista/utils/config.js
@@ -5,8 +5,9 @@ const PORT = process.env.PORT || 3003
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI || 'mongodb+srv://mokomuru:kissa123@cluster0.in0dxw7.mongodb.net/bloglist?retryWrites=true&w=majority'
+    : process.env.MONGODB_URI || 'mongodb+srv://mokomuru:<password>@cluster0.in0dxw7.mongodb.net/bloglist?retryWrites=true&w=majority'
 
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`
+
 
 module.exports = { MONGODB_URI, PORT, BASE_URL }
